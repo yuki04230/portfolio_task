4dcf59c1be2e3a13228929d5b39f05ad2c02ec71
yuki04230 <yuki.1395.8620@gmail.com>
2021-11-14 14:03:07 +0900

    3からのこみっと

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -15,5 +15,9 @@ class PostController extends Controller
     {
          return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    public function show(Post $post)
+{
+    return view('show')->with(['post' => $post]);
+}
 }
 ?>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,6 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
 Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');
